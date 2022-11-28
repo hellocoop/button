a346bb66472b2150f4c07461192c99f52e1b1ff2
rohanharikr <rohanharikumar80@gmail.com>
2022-11-28 20:00:29 +0400

    button style, hover, and custom title

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,9 +4,15 @@ import HelloButton from './HelloButton.jsx'
 import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(false)
   return (
     <HelloButton
-      onClick={() => alert(123)}
+      onClick={() => setLoading(!loading)}
+      loading={loading}
+      tooltip={true}
+      style="hello-btn-white-on-dark"
+      hoverStyle="hello-btn-hover-flare"
+      language="fr"
     />
   )
 }
--- react/src/HelloButton.jsx
+++ react/src/HelloButton.jsx
@@ -1,19 +1,31 @@
 import React, {useState} from "react"
+import { useEffect } from "react"
+import "../../hello-btn.css"
 
 const HelloButton = ({
     onClick = () => {},
     loading = false,
-    tooltip = true
+    tooltip = true,
+    style = "",
+    hoverStyle = "",
+    language = "en",
+    customTitle = ""
 }) => {
     const [showTooltip, setShowTooltip] = useState(false)
+    useEffect(() => {
+        document.addEventListener("click", (e) => {
+            //hande click outside
+        })
+    }, [])
+
     return (
         <div className="hello-container">
             <button
                 onClick={onClick}
                 disabled={loading} 
-                className="hello-btn"
+                className={`hello-btn ${loading ? "hello-btn-loader" : ""} ${style || ""} ${hoverStyle || ""}`}
             >
-                ō&nbsp;&nbsp;&nbsp;Continue with Hellō
+                {customTitle || "ō   Continue with Hellō"}
             </button>
             {tooltip && (
                 <div className="hello-about-container">
@@ -25,7 +37,7 @@ const HelloButton = ({
                     </button>
                     {
                         showTooltip && (
-                            <span className="hello-about-bubble" style={{visibility: "visible"}}>
+                            <span className="hello-about-bubble" style={{visibility: "visible", top: "46px"}}>
                                 Hellō is a personal identity wallet that lets you choose what you share, how you authenticate, and how you recover your wallet. Hellō remembers who you are and protects your privacy.
                             </span>
                         )
