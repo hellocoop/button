ca9cbb1030ef4e5428a5ef91cc2b7afdc5934007
rohanharikr <rohanharikumar80@gmail.com>
2022-11-28 19:45:33 +0400

    about bubble

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <HelloButton
       onClick={() => alert(123)}
-      tooltip={false}
     />
   )
 }
--- react/src/HelloButton.jsx
+++ react/src/HelloButton.jsx
@@ -7,7 +7,7 @@ const HelloButton = ({
 }) => {
     const [showTooltip, setShowTooltip] = useState(false)
     return (
-        <div>
+        <div className="hello-container">
             <button
                 onClick={onClick}
                 disabled={loading} 
@@ -16,12 +16,21 @@ const HelloButton = ({
                 ō&nbsp;&nbsp;&nbsp;Continue with Hellō
             </button>
             {tooltip && (
-                <button
-                    onClick={() => setShowTooltip(status => !status)}
-                    className="hello-about"
-                >
-                    Hellō lets you control your identity
-                </button>
+                <div className="hello-about-container">
+                    <button
+                        onClick={() => setShowTooltip(status => !status)}
+                        className="hello-about"
+                    >
+                        Hellō lets you control your identity
+                    </button>
+                    {
+                        showTooltip && (
+                            <span className="hello-about-bubble" style={{visibility: "visible"}}>
+                                Hellō is a personal identity wallet that lets you choose what you share, how you authenticate, and how you recover your wallet. Hellō remembers who you are and protects your privacy.
+                            </span>
+                        )
+                    }
+                </div>
             )}
         </div>
     )
